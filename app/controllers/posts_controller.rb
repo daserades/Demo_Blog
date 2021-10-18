109e0df1e695b6b6d5e83404a6d2817cf82e828b
Anil Yilmaz <anlylmaz.92@gmail.com>
2021-10-18 18:52:45 +0300

    Finish ımplementation of blog functionality

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,8 +1,51 @@
 class PostsController < ApplicationController
     def index
-    end
-    def new
-    end
-    def create
-    end
+		@posts = Post.all.order("created_at DESC")
+	end
+
+	def new
+		@post = Post.new
+	end
+
+	def create
+		@post = Post.new(post_params)
+
+		if @post.save
+			redirect_to @post
+		else
+			render 'new'
+		end
+	end
+
+	def show
+	end
+
+	def update
+
+		if @post.update(post_params)
+			redirect_to @post
+		else
+			render 'edit'
+		end
+	end
+
+	def edit
+		@post = Post.find(params[:id])
+	end
+
+	def destroy
+		@post = Post.find(params[:id])
+		@post.destroy
+
+		redirect_to posts_path
+
+	end
+
+	private
+
+	def post_params
+		params.require(:post).permit(:title, :content)
+	end
+
+	
 end
